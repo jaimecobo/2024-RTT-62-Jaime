cc59c88c0b27030b1070951f8214d44663b1b33f
Jaime <jaimecobo@outlook.com>
2024-05-01 16:01:46 -0500

    First exercise completed

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
@@ -5,5 +5,10 @@ public class JavaBasicsClass {
 
         /* Write a program that declares two integer variables, assigns an integer to each, and adds them together.
            Assign the sum to a variable. Print out the result. */
+        int num1 = 3, num2 = 5;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        System.out.println("###################################################################### \n");
+
     }
 }
