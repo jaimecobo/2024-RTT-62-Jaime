4be4cbf8ebe25b94b1217519c72e1df599e565c7
Jaime <jaimecobo@outlook.com>
2024-05-01 16:01:10 -0500

    First exercise instruction

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
@@ -1,5 +1,9 @@
 package com.perscholas.java_basics;
 
 public class JavaBasicsClass {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        /* Write a program that declares two integer variables, assigns an integer to each, and adds them together.
+           Assign the sum to a variable. Print out the result. */
+    }
 }
