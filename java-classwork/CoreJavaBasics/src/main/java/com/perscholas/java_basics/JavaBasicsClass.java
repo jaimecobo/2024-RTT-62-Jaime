3d8b9ed70913165485a921e1b30d44311f96d4d8
Jaime <jaimecobo@outlook.com>
2024-05-01 16:06:35 -0500

    Sixth exercise instruction

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/JavaBasicsClass.java
@@ -52,5 +52,9 @@ public class JavaBasicsClass {
         System.out.println("###################################################################### \n");
 
 
+        /* Write a program that declares two integer variables, x and y, and assigns the number 5 to x and the number 6 to y.
+           Declare a variable q and assign y/x to it and print q. Now, cast y to a double and assign it to q. Print q again. */
+
+
     }
 }
