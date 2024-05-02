9fefedef2b7c2b4965d578efd0eb6173b57d93b3
Jaime <jaimecobo@outlook.com>
2024-05-02 09:30:28 -0500

    Message and input

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/ScannerExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/ScannerExample.java
@@ -6,5 +6,8 @@ public class ScannerExample {
     public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int num1 = sc1.nextInt();
+
     }
 }
