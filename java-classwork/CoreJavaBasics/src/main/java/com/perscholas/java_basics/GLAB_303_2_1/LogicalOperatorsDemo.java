21a0682ef1ff3220f47d0aafdd55b58bb316d81c
Jaime <jaimecobo@outlook.com>
2024-05-02 21:38:27 -0500

    GLAB_303_2_1\LogicalOperatorsDemo.java

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_2_1/LogicalOperatorsDemo.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_2_1/LogicalOperatorsDemo.java
@@ -1,2 +1,16 @@
-package com.perscholas.java_basics.GLAB_303_2_1;public class LogicalOperatorsDemo {
+package com.perscholas.java_basics.GLAB_303_2_1;
+
+public class LogicalOperatorsDemo {
+    public static void main(String[] args)
+    {
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("x ^ y : " + (x ^ y));
+        System.out.println("!x : " + (!x));
+    }
+
 }
