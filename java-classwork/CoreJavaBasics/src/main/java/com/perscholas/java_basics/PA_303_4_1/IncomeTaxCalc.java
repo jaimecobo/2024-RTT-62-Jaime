ff5ac63a891ba8baebb163b340bf527d7cff9f4a
Jaime <jaimecobo@outlook.com>
2024-05-03 21:38:49 -0500

    Income tax calculator

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IncomeTaxCalc.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IncomeTaxCalc.java
@@ -1,2 +1,13 @@
-package com.perscholas.java_basics.PA_303_4_1;public class IncomeTaxCalc {
+package com.perscholas.java_basics.PA_303_4_1;
+
+public class IncomeTaxCalc {
+    /*
+    7. Create a program that lets the users input their filing status and income. Display how much tax the user would have to pay according to status and income.
+    The U.S. federal personal income tax is calculated based on the filing status and taxable income.
+    There are four filing statuses: Single, Married Filing Jointly, Married Filing Separately, and Head of Household.
+    The tax rates for 2009 are shown below.
+     */
+    public static void main(String[] args) {
+
+    }
 }
