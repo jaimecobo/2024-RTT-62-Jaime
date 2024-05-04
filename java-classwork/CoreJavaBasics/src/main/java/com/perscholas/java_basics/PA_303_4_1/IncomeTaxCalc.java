1b733a1c6be11ad04cebfe35d7c90ae8e893f97d
Jaime <jaimecobo@outlook.com>
2024-05-04 17:22:44 -0500

    Declared Scanner/input interface for IncomeTaxCalc

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IncomeTaxCalc.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IncomeTaxCalc.java
@@ -1,5 +1,7 @@
 package com.perscholas.java_basics.PA_303_4_1;
 
+import java.util.Scanner;
+
 public class IncomeTaxCalc {
     /*
     7. Create a program that lets the users input their filing status and income. Display how much tax the user would have to pay according to status and income.
@@ -8,6 +10,19 @@ public class IncomeTaxCalc {
     The tax rates for 2009 are shown below.
      */
     public static void main(String[] args) {
+        double income = 1000000;
+        double incomeTax = 0;
+        int status = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number corresponding to your status, as described bellow." +
+                "\n 1) Single" +
+                "\n 2) Married Filing Jointly or Qualifying Widow(er)" +
+                "\n 3) Married Filing Separately" +
+                "\n 4) Head of Household");
+
+        status = sc.nextInt();
 
     }
 }
+
