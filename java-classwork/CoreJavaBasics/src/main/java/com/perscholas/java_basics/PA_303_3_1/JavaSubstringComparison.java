7e081fc651df9f62cb9dffacab96064b0b5761ed
Jaime <jaimecobo@outlook.com>
2024-05-04 17:17:38 -0500

    Java substring comparison

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_3_1/JavaSubstringComparison.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_3_1/JavaSubstringComparison.java
@@ -35,32 +35,37 @@ public class JavaSubstringComparison {
     String s="welcometojava"  has the following lexicographically-ordered substrings of length k=3:
         ["ava", "com", "elc", "eto", "jav", "lco", "met", "oja", "ome", "toj", "wel"]
     We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring
-    as two newline-separated values (i.e., ava\nwel).
+    as two newline-separated values (i.e., ava\wel).
 
     The stub code in the editor then prints ava as our first line of output and wel as our second line of output.
     */
 
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
-
-        // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
-//        String result = smallest.compareTo(largest) >= 1 ? "Yes" : "No";
-
+        String carrier = s.substring(0,k);
+        String smallest = carrier;
+        String largest = carrier;
+        for(int i = 1; i < s.length() -k +1; i++) {
+            carrier = s.substring(i, i+k);
+            smallest = smallest.compareTo(carrier) >= 1 ? carrier: smallest;
+            largest = largest.compareTo(carrier) < 0 ? carrier : largest;
+        }
 
         return smallest + "\n" + largest;
+
     }
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your string:");
         String s = scan.next();
+        System.out.println("Enter your number:");
         int k = scan.nextInt();
         scan.close();
-
         System.out.println(getSmallestAndLargest(s, k));
+
     }
 }
 /*
