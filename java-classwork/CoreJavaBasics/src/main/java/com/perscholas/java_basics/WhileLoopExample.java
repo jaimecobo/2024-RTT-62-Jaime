79f3a51ade93bda8156f1e25c1fecb3af6205cca
Jaime <jaimecobo@outlook.com>
2024-05-03 21:22:04 -0500

    While loop example

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/WhileLoopExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/WhileLoopExample.java
@@ -1,2 +1,11 @@
-package com.perscholas.java_basics;public class WhileLoopExample {
+package com.perscholas.java_basics;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int x = 1;
+        while(x < 10){
+            x = x + 1;
+            System.out.println(x);
+        }
+    }
 }
