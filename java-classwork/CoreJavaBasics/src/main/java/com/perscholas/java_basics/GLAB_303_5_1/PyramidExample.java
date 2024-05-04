6f132cd8a588d3770a12b32a520f7b85bed2c1fa
Jaime <jaimecobo@outlook.com>
2024-05-03 21:29:12 -0500

    Sum natural numbers

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_5_1/PyramidExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_5_1/PyramidExample.java
@@ -1,2 +1,13 @@
-package com.perscholas.java_basics.GLAB_303_5_1;public class PyramidExample {
+package com.perscholas.java_basics.GLAB_303_5_1;
+
+public class PyramidExample {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();//new line
+        }
+    }
+
 }
