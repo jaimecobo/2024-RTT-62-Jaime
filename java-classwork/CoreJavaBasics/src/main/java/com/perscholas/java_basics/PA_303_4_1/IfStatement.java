304c22086b52d68b963b0ffb76dba22216d83646
Jaime <jaimecobo@outlook.com>
2024-05-03 21:35:12 -0500

    .\if statement

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IfStatement.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_4_1/IfStatement.java
@@ -1,2 +1,15 @@
-package com.perscholas.java_basics.PA_303_4_1;public class IfStatement {
+package com.perscholas.java_basics.PA_303_4_1;
+
+public class IfStatement {
+    /*
+    1. Write a program that declares 1 integer variable x, and then assigns 7 to it.
+    Write an if statement to print out “Less than 10” if x is less than 10.
+    Change x to equal 15, and notice the result (console should not display anything).
+    */
+    public static void main(String[] args) {
+        int num = 15;
+        if(num < 10){
+            System.out.println("Less than 10");
+        }
+    }
 }
