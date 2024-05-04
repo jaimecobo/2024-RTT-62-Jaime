d5b7e43e988f85573e517150857f51cb85201011
Jaime <jaimecobo@outlook.com>
2024-05-03 21:20:57 -0500

    Switch statement example

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/SwitchStatementExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/SwitchStatementExample.java
@@ -1,2 +1,28 @@
-package com.perscholas.java_basics;public class SwitchStatementExample {
+package com.perscholas.java_basics;
+
+public class SwitchStatementExample {
+    public static void main(String args[]) {
+        String output;
+        // grade is a value 0 - 100
+        int grade = 80;
+        switch (grade / 10) {
+            case 10:
+                output = "Wow!; ";
+                break;
+            case 9:
+                output = "Excellent!";
+                break;
+            case 8:
+                output = "Very Good!";
+                break;
+            case 7:
+                output = "Good!";
+                break;
+            default:
+                output = "Keep trying!";
+                break;
+        }
+        System.out.println(output);
+    }
+
 }
