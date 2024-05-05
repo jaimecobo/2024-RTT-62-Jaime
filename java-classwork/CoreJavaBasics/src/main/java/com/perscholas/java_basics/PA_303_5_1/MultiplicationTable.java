faa2221be2f923abbcff0ee9c479bf5e40c16524
Jaime <jaimecobo@outlook.com>
2024-05-05 17:53:44 -0500

    Multiplication table [Draws header]

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
@@ -6,5 +6,15 @@ public class MultiplicationTable {
     */
     public static void main(String[] args) {
         System.out.print(" ");
+        for (int line = 0; line <= 51; line++) {
+            if(line > 50){
+                System.out.println();
+                if(line == 51){
+                    System.out.println("|                MULTIPLICATION TABLE               |");
+                }
+            } else{
+                System.out.print("_");
+            }
+        }
     }
 }
