134e2b077fb07dbe344ca67747ea5a8789d9c6e7
Jaime <jaimecobo@outlook.com>
2024-05-05 17:52:56 -0500

    Multiplication table [sets initial drawing point]

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
@@ -1,11 +1,10 @@
 package com.perscholas.java_basics.PA_303_5_1;
 
-import java.sql.SQLOutput;
-
 public class MultiplicationTable {
     /*
     Write a program that uses nested for loops to print a multiplication table.
     */
     public static void main(String[] args) {
+        System.out.print(" ");
     }
 }
