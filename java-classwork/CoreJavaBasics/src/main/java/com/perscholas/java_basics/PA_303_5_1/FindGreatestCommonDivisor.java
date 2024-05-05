e25e3d5f0924970a468a41d632c8694d965ac15f
Jaime <jaimecobo@outlook.com>
2024-05-05 17:58:26 -0500

    Find Great Common Divisor [comment + main]

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/FindGreatestCommonDivisor.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/FindGreatestCommonDivisor.java
@@ -1,2 +1,16 @@
-package com.perscholas.java_basics.PA_303_5_1;public class FindGreatestCommonDivisor {
+package com.perscholas.java_basics.PA_303_5_1;
+
+public class FindGreatestCommonDivisor {
+    /*
+    Write a program that prompts the user to enter two positive integers, and find their greatest common divisor (GCD).
+    Examples:
+    Enter 2 and 4. The gcd is 2.
+    Enter 16 and 24.  The gcd is 8.
+    How do you find the gcd?
+    Name the two input integers n1 and n2.
+    You know number 1 is a common divisor, but it may not be the gcd.
+    Check whether k (for k = 2, 3, 4, and so on) is a common divisor for n1 and n2, until k is greater than n1 or n2.
+    */
+    public static void main(String[] args) {
+    }
 }
