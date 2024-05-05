0e16acdcc0799ea9e275b855684878407237b646
Jaime <jaimecobo@outlook.com>
2024-05-05 17:51:39 -0500

    Multiplication table [comment + main]

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_5_1/MultiplicationTable.java
@@ -1,2 +1,11 @@
-package com.perscholas.java_basics.PA_303_5_1;public class MultiplicationTable {
+package com.perscholas.java_basics.PA_303_5_1;
+
+import java.sql.SQLOutput;
+
+public class MultiplicationTable {
+    /*
+    Write a program that uses nested for loops to print a multiplication table.
+    */
+    public static void main(String[] args) {
+    }
 }
