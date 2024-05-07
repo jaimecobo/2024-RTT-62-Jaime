b08d7fb23c54a80cb1a6c148545efe03903cbf47
Jaime <jaimecobo@outlook.com>
2024-05-06 19:51:51 -0500

    Array Sort Min Max

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArraySortMinMax.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArraySortMinMax.java
@@ -1,2 +1,21 @@
-package com.perscholas.java_basics.PA_303_7_1;public class ArraySortMinMax {
+package com.perscholas.java_basics.PA_303_7_1;
+
+import java.util.Arrays;
+
+public class ArraySortMinMax {
+    /*
+    Write a program to sort the following int array in ascending order: {4, 2, 9, 13, 1, 0}.
+    Print the array in ascending order, and print the smallest and the largest element of the array.
+    The output will look like the following:
+    Array in ascending order: 0, 1, 2, 4, 9, 13
+    The smallest number is 0
+    The biggest number is 13
+    */
+    public static void main(String[] args) {
+        int[] numbers = new int[] {4, 2, 9, 13, 1, 0};
+        Arrays.sort(numbers);
+        System.out.println("Array in ascending order: " + Arrays.toString(numbers));
+        System.out.println("The smallest number is " + numbers[0]);
+        System.out.println("The biggest number is " + numbers[numbers.length-1]);
+    }
 }
