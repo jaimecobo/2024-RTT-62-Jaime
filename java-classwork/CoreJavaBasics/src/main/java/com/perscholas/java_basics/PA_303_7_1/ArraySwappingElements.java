3e05a11297f06c23105fa122d9d519410795d5d7
Jaime <jaimecobo@outlook.com>
2024-05-06 19:52:17 -0500

    Array Swapping Elements

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArraySwappingElements.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArraySwappingElements.java
@@ -1,9 +1,16 @@
 package com.perscholas.java_basics.PA_303_7_1;
 
+import java.util.Arrays;
+
 public class ArraySwappingElements {
     /*
-    Task 8: Write a program that creates a String array of 5 elements and swaps the first element with the middle element without creating a new array.
+    Write a program that creates a String array of 5 elements and swaps the first element with the middle element without creating a new array.
     */
     public static void main(String[] args) {
+        String[] positions = new String[]{"First", "Second", "Third", "Fourth", "Fifth"};
+        String carrier = positions[0];
+        positions[0] = positions[positions.length/2];
+        positions[positions.length/2] = carrier;
+        System.out.println(Arrays.toString(positions));
     }
 }
