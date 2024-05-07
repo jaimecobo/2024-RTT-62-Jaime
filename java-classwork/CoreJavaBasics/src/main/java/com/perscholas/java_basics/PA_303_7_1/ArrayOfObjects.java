783f6b46116dfb1445ba60fa5041216e6d0b4676
Jaime <jaimecobo@outlook.com>
2024-05-06 19:51:17 -0500

    Array Of Objects

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArrayOfObjects.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/ArrayOfObjects.java
@@ -1,2 +1,13 @@
-package com.perscholas.java_basics.PA_303_7_1;public class ArrayOfObjects {
+package com.perscholas.java_basics.PA_303_7_1;
+
+import java.util.Arrays;
+
+public class ArrayOfObjects {
+    /*
+        Task 10: Create an array that includes an integer, 3 strings, and 1 double. Print the array.
+    */
+    public static void main(String[] args) {
+        Object[] objects = new Object[] {8, "Java", "Programming", "Language", 22.2};
+        System.out.println(Arrays.toString(objects));
+    }
 }
