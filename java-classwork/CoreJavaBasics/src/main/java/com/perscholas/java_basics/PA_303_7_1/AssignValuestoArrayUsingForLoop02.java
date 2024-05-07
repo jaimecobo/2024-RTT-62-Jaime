f8a7599dae5805259557ea96144d846f68574397
Jaime <jaimecobo@outlook.com>
2024-05-06 19:57:26 -0500

    Assign values to array using for loop 02

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/AssignValuestoArrayUsingForLoop02.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/AssignValuestoArrayUsingForLoop02.java
@@ -1,2 +1,17 @@
-package com.perscholas.java_basics.PA_303_7_1;public class AssignValuestoArrayUsingForLoop02 {
+package com.perscholas.java_basics.PA_303_7_1;
+
+import java.util.Arrays;
+
+public class AssignValuestoArrayUsingForLoop02 {
+    /*
+    Write a program where you create an integer array of 5 numbers.
+    Loop through the array and assign the value of the loop control variable multiplied by 2 to the corresponding index in the array.
+    */
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for(int i=1; i < numbers.length; i++){
+            numbers[i] = i*2;
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
 }
