6e7072b62799c5a64d2b77cedb5818b8110cd506
Jaime <jaimecobo@outlook.com>
2024-05-06 19:44:46 -0500

    Array Insert

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides303_6/ArrayInsert.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides303_6/ArrayInsert.java
@@ -1,2 +1,14 @@
-package com.perscholas.java_basics.slides303_6;public class ArrayInsert {
+package com.perscholas.java_basics.slides303_6;
+
+public class ArrayInsert {
+    int[] arrayOne = {1, 2, 3, 4, 5};
+
+
+    // Write som code to insert a 9 in arrayOne position 2
+
+    // 1) Creates a new array of size arrayOne.length + 1
+    int[] arrayTwo = new int[arrayOne.length + 1];
+
+    // 2) Copy the first part of arrayOne (before position 2) to arrayTwo
+//    for(int pos = )
 }
