dab1015da234091c9292b40fbab0e118cc213f4a
Jaime <jaimecobo@outlook.com>
2024-05-06 20:01:25 -0500

    Skip Array element

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/SkipArrayElement.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_7_1/SkipArrayElement.java
@@ -1,2 +1,18 @@
-package com.perscholas.java_basics.PA_303_7_1;public class SkipArrayElement {
+package com.perscholas.java_basics.PA_303_7_1;
+
+import java.util.Arrays;
+
+public class SkipArrayElement {
+    /*
+    Write a program where you create an array of 5 elements.
+    Loop through the array and print the value of each element, except for the middle (index 2) element.
+     */
+    public static void main(String[] args) {
+        int[] numbers = new int[]{0, 3, 6, 9, 12};
+        for(int i=0; i < numbers.length; i++){
+            if(i != numbers.length/2){
+                System.out.println(numbers[i]);
+            }
+        }
+    }
 }
