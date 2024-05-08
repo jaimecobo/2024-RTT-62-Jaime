f97bf94be97edc2bf11e0b6fa6f24247796db6ba
Jaime <jaimecobo@outlook.com>
2024-05-08 15:17:25 -0500

    Circle Class

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Circle.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Circle.java
@@ -1,2 +1,21 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class Circle {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+
+public class Circle extends Shape {
+
+
+    private double diameter;
+
+    public double calculateArea() {
+        area = 456;
+        return Math.PI * (diameter / 2) * (diameter / 2);
+    }
+
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
 }
