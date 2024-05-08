7b5cc5895a9eb129393c2b9bf9b2734c25b709d6
Jaime <jaimecobo@outlook.com>
2024-05-08 15:17:08 -0500

    Shape Class

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Shape.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Shape.java
@@ -1,2 +1,31 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class Shape {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+public class Shape {
+    public static double area;
+
+    private String name;
+
+    public Shape() {
+
+    }
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    protected void printName() {
+        System.out.println("The name of the shape is " + name);
+    }
+
+    //public abstract double calculateArea();       Shape is not abstract and does not override abstract method calculateArea()
+    public double calculateArea(){
+        return 0;
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
