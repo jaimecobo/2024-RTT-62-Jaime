50cd6894130fa5e6c4a6bcc10091c3a60c5b2ec0
Jaime <jaimecobo@outlook.com>
2024-05-08 15:05:12 -0500

    CoffeeShop

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/coffeeShop/CoffeeShop.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/coffeeShop/CoffeeShop.java
@@ -1,2 +1,33 @@
-package com.perscholas.java_basics.coffeeShop;public class CoffeeShop {
+package com.perscholas.java_basics.coffeeShop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoffeeShop {
+    private List<Product> products = new ArrayList<>();
+
+    // building the menu of products for sale at the coffee shop
+    public void initProducts() {
+        Product p1 = new Product();
+        p1.setPrice(5.46);
+        p1.setName("Small Coffee");
+        p1.setBeverage(true);
+        products.add(p1);
+
+        Product largeCoffee = new Product();
+        largeCoffee.setPrice(9.46);
+        largeCoffee.setName("Large Coffee");
+        largeCoffee.setBeverage(true);
+        products.add(largeCoffee);
+
+        Product cakePop = new Product();
+        cakePop.setPrice(4.89);
+        cakePop.setName("Cake Pop");
+        cakePop.setBeverage(false);
+        products.add(cakePop);
+
+        Product eggSandwich = new Product(7.89, "Egg Sandwich", false, 50);
+        products.add(eggSandwich);
+    }
+
 }
