54242c70b426c576ce52f429cf7df4b32909959f
Jaime <jaimecobo@outlook.com>
2024-05-08 15:19:53 -0500

    Main method for Shapes program

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/MyMain.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/MyMain.java
@@ -1,2 +1,24 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class mymain {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyMain {
+    public static double PI = 3.14159;
+
+    public static void main(String[] args ) {
+        List<Shape> shapes = new ArrayList<>();
+
+        Circle c = new Circle();
+        c.setName("Circle");
+        c.setDiameter(10.0);
+
+        shapes.add(c);
+
+        for ( Shape shape : shapes ) {
+            System.out.println("The area of " + shape.getName() + " equals " + shape.calculateArea());
+        }
+
+    }
+
 }
