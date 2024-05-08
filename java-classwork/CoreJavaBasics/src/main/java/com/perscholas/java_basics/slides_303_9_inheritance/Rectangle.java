e537372081d8479a126598ae27dedad3e2ef05d4
Jaime <jaimecobo@outlook.com>
2024-05-08 15:17:50 -0500

    Rectangle Class

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Rectangle.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Rectangle.java
@@ -1,2 +1,42 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class Rectangle {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+
+public class Rectangle extends Shape {
+
+    private int length;
+    private int width;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(String name) {
+        super(name);
+    }
+
+    protected void printName() {
+        System.out.println("Rectangle is named rectangle ... rectangle mcrectface");
+    }
+
+    @Override
+    public double calculateArea() {
+        super.printName();
+        area = 123;
+        return length * width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
