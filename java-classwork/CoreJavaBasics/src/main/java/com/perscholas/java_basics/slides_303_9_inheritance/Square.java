a458e6bca2c0c9daa4d1afd61ebcd5444d6323c1
Jaime <jaimecobo@outlook.com>
2024-05-08 15:18:07 -0500

    Square Class

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Square.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Square.java
@@ -1,2 +1,17 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class Square {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+public class Square extends Shape{
+    private int width;
+
+    @Override
+    public double calculateArea() {
+        return width * width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
