76341f10a5448b0026564e2f3e2109ec06c2461d
Jaime <jaimecobo@outlook.com>
2024-05-08 15:24:40 -0500

    DemoOne - demo about static

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/DemoOne.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/DemoOne.java
@@ -1,2 +1,7 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class DemoOne {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+public class DemoOne {
+    static int var1 = 20;
+    static String var2 = "perscholas";
+    int var3 = 100;
+
 }
