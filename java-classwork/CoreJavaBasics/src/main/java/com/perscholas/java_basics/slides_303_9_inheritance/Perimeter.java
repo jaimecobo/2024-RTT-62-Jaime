94dc498b62c49e26c66fbf74784463bbe43c345f
Jaime <jaimecobo@outlook.com>
2024-05-09 20:41:38 -0500

    Perimeter

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Perimeter.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/Perimeter.java
@@ -1,2 +1,9 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public interface Perimeter {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+
+public interface Perimeter {
+
+    public static final double CONST = 1.618;
+
+    public double calculatePerimeter();
+
 }
