43d7bec6ab178068ec844de1cd9568d1d308d85e
Jaime <jaimecobo@outlook.com>
2024-05-09 20:42:33 -0500

    OuterClass

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/OuterClass.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_9_inheritance/OuterClass.java
@@ -1,2 +1,15 @@
-package com.perscholas.java_basics.slides_303_9_inheritance;public class OuterClass {
+package com.perscholas.java_basics.slides_303_9_inheritance;
+
+public class OuterClass {
+    public OuterClass(){
+        InnerClass ic = new InnerClass();
+    }
+
+
+
+    private class InnerClass {
+        int x;
+        int y;
+        int z;
+    }
 }
