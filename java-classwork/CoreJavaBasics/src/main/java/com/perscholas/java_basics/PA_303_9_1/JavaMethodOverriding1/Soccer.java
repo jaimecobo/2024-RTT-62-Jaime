efd1e9ed2e1bd4f5741d9eccdaee6322d249b166
Jaime <jaimecobo@outlook.com>
2024-05-09 21:07:37 -0500

    PA_303_9_1 -> Java method Overriding 1

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_9_1/JavaMethodOverriding1/Soccer.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_9_1/JavaMethodOverriding1/Soccer.java
@@ -1,2 +1,15 @@
-package com.perscholas.java_basics.PA_303_9_1.JavaMethodOverriding1;public class Soccer {
+package com.perscholas.java_basics.PA_303_9_1.JavaMethodOverriding1;
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+
 }
--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_9_1/JavaMethodOverriding1/Sports.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/PA_303_9_1/JavaMethodOverriding1/Sports.java
@@ -1,2 +1,12 @@
-package com.perscholas.java_basics.PA_303_9_1.JavaMethodOverriding1;public class Sports {
+package com.perscholas.java_basics.PA_303_9_1.JavaMethodOverriding1;
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
 }
