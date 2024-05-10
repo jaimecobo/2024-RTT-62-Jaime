f5593703b989c5e86b0ce7665dd4a59c70d19291
Jaime <jaimecobo@outlook.com>
2024-05-09 20:43:16 -0500

    SBA1 Practice

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/SBA1Practice.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/SBA1Practice.java
@@ -1,2 +1,51 @@
-package com.perscholas.java_basics;public class SBA1Practice {
+package com.perscholas.java_basics;
+
+public class SBA1Practice {
+    public static void main(String[] args) {
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // part 1 - create a for loop to print each character of the string s on its own line
+        // you can use the s.charAt(pos) function
+        for ( int pos = 0 ; pos < s.length() ; pos++) {
+            System.out.println(s.charAt(pos));
+        }
+
+        /*
+          A
+          B
+          C
+          D
+          E
+          F
+          G
+          ...
+         */
+
+        // part 2
+        // Given the following Strings convert them to integer values   Integer.parseInt()
+        String ten = "10";
+        String hundred = "100";
+
+        int ten1 = Integer.valueOf(ten);
+        int hun = Integer.parseInt(hundred);
+
+        // part 3
+        // given an integer array find the sum of all the integers in the array
+        int[] array = {2, 4, 5, 6, 7, 8, 10};
+        int sum = 0;
+        for ( int pos = 0 ; pos < array.length ; pos++) {
+            sum = sum + array[pos];
+        }
+        System.out.println(sum);
+
+        // part 4
+        // given the array of string covert them to numbers and print the sum
+        String[] nums = {"2", "4", "5", "6", "7", "8", "10"};
+        int sum1 = 0;
+        for ( String string : nums ) {
+            int value = Integer.valueOf(string);
+            sum1 = sum1 + value;
+        }
+        System.out.println(sum1);
+    }
 }
