1bbbc2306e7bcda5dd753048f1591ca1d7c2bc88
Jaime <jaimecobo@outlook.com>
2024-05-12 15:44:16 -0500

    Search elements in ArrayList example

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_2/SearchElementsInArrayListExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_2/SearchElementsInArrayListExample.java
@@ -1,2 +1,37 @@
-package com.perscholas.java_basics.GLAB_303_11_2;public class SearchElementsInArrayListExample {
+package com.perscholas.java_basics.GLAB_303_11_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchElementsInArrayListExample {
+/*
+Searching for Elements in an ArrayList
+The example below shows how to:
+Check if an ArrayList contains a given element | contains().
+Find the index of the first occurrence of an element in an ArrayList | indexOf().
+Find the index of the last occurrence of an element in an ArrayList | lastIndexOf().
+*/
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Steve");
+        names.add("John");
+        names.add("Steve");
+        names.add("Maria");
+
+        // Check if an ArrayList contains a given element
+        System.out.println("Does names array contain \"Bob\"? : " + names.contains("Bob"));
+
+        // Find the index of the first occurrence of an element in an ArrayList
+        System.out.println("indexOf \"Steve\": " + names.indexOf("Steve"));
+        System.out.println("indexOf \"Mark\": " + names.indexOf("Mark"));
+
+        // Find the index of the last occurrence of an element in an ArrayList
+        System.out.println("lastIndexOf \"John\" : " + names.lastIndexOf("John"));
+        System.out.println("lastIndexOf \"Bill\" : " + names.lastIndexOf("Bill"));
+    }
 }
+
+
