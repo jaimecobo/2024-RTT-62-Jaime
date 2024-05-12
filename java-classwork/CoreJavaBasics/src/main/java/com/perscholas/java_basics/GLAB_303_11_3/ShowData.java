223acb0473d8ea3b43378aefb4272e234e25eb97
Jaime <jaimecobo@outlook.com>
2024-05-12 15:49:27 -0500

    ShowData, Displays ArrayList Books

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_3/ShowData.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_3/ShowData.java
@@ -1,2 +1,18 @@
-package com.perscholas.java_basics.GLAB_303_11_3;public class showData {
+package com.perscholas.java_basics.GLAB_303_11_3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+public class ShowData {
+    public static void main(String[] args) {
+        // instantiation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> myBookList = b.bookdetails();
+        for(Book showValue: myBookList)
+        {
+            // ---- invoking getter method for geting Data---------
+            System.out.println(showValue.getNumber() + " " + showValue.getName() +" "+ showValue.getCategory() +" "+ showValue.getAuthor());
+        }
+    }
 }
+
+
