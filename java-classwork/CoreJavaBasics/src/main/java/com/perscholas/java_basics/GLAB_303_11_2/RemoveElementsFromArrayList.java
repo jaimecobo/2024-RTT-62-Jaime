fb5c85c2e3e988fbf9174f19e379d44316eb0dfe
Jaime <jaimecobo@outlook.com>
2024-05-12 15:43:37 -0500

    Remove elements from ArrayList

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_2/RemoveElementsFromArrayList.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_2/RemoveElementsFromArrayList.java
@@ -1,2 +1,52 @@
-package com.perscholas.java_basics.GLAB_303_11_2;public class RemoveElementsFromArrayList {
+package com.perscholas.java_basics.GLAB_303_11_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class RemoveElementsFromArrayList {
+/*
+Removing Elements from an ArrayList
+This example shows:
+How to remove an element at a given index in an ArrayList | remove(int index).
+How to remove an element from an ArrayList | remove(Object o).
+How to remove all of the elements from an ArrayList that exist in a given collection | removeAll().
+How to remove all of the elements matching a given predicate | removeIf().
+How to clear an ArrayList | clear().
+*/
+    public static void main(String[] args) {
+        List<String> programmingLanguages = new ArrayList<>();
+        programmingLanguages.add("C");
+        programmingLanguages.add("C++");
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Kotlin");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Perl");
+        programmingLanguages.add("Ruby");
+
+        System.out.println("Initial List: " + programmingLanguages);
+
+        // Remove the element at index `5`
+        programmingLanguages.remove(5);
+        System.out.println("After remove(5): " + programmingLanguages);
+
+        // Remove the first occurrence of the given element from the ArrayList
+        // (The remove() method returns false if the element does not exist in the ArrayList)
+        boolean isRemoved = programmingLanguages.remove("Kotlin");
+        System.out.println("After remove(\"Kotlin\"): " + programmingLanguages);
+
+        // Remove all the elements that exist in a given collection
+        List<String> scriptingLanguages = new ArrayList<>();
+        scriptingLanguages.add("Python");
+        scriptingLanguages.add("Ruby");
+        scriptingLanguages.add("Perl");
+
+        programmingLanguages.removeAll(scriptingLanguages);
+        System.out.println("After removeAll(scriptingLanguages): " + programmingLanguages);
+
+        // Remove all elements from the ArrayList
+        programmingLanguages.clear();
+        System.out.println("After clear(): " + programmingLanguages);
+    }
 }
+
