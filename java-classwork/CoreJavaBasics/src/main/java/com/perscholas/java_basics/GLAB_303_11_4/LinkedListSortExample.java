69917ce2e01b14d8edeb673e1ad30fbce591fb1c
Jaime <jaimecobo@outlook.com>
2024-05-12 15:51:25 -0500

    LinkedList Collections.sort example

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_4/LinkedListSortExample.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_4/LinkedListSortExample.java
@@ -1,2 +1,37 @@
-package com.perscholas.java_basics.GLAB_303_11_4;public class LinkedListsortExample {
+package com.perscholas.java_basics.GLAB_303_11_4;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class LinkedListSortExample {
+/*
+How to Sort LinkedList
+In this example, we will demonstrate how to sort a LinkedList using the Collections.sort() method.
+Please note that for custom sorting of objects, we can use Collections.sort(linkedList, comparator) method.
+*/
+    public static void main(String[] args)
+    {
+        //Create linked list
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("C");
+        linkedList.add("B");
+        linkedList.add("D");
+
+//Unsorted
+        System.out.println(linkedList);
+
+//1. Sort the list
+        Collections.sort(linkedList);
+
+//Sorted
+        System.out.println(linkedList);
+
+//2. Custom sorting
+        Collections.sort(linkedList, Collections.reverseOrder());
+
+//Custom sorted
+        System.out.println(linkedList);
+    }
+
 }
