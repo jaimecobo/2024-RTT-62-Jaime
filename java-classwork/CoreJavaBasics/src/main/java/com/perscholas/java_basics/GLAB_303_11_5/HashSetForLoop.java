ed5eda9a7aba81aecfa2152f7e7519d04b693e74
Jaime <jaimecobo@outlook.com>
2024-05-12 16:01:31 -0500

    HashSetForLoop, Fruits HashSet to iterate using for loop

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/HashSetForLoop.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/HashSetForLoop.java
@@ -1,2 +1,27 @@
-package com.perscholas.java_basics.GLAB_303_11_5;public class HashSetForLoop {
+package com.perscholas.java_basics.GLAB_303_11_5;
+
+import java.util.HashSet;
+
+public class HashSetForLoop {
+/*
+Remember: HashSet DOES NOT maintain any order, so this order might be changed if you run your code a second time.
+ */
+    public static void main(String args[]) {
+        // HashSet declaration
+        HashSet<String> hset = new HashSet<String>();
+
+        // Adding elements to the HashSet
+        hset.add("Apple");
+        hset.add("Mango");
+        hset.add("Grapes");
+        hset.add("Orange");
+        hset.add("Fig");
+        //Addition of duplicate elements
+        hset.add("Apple");
+        hset.add("Mango");
+        //Addition of null values
+        hset.add(null);
+        hset.add(null);
+    }
+
 }
