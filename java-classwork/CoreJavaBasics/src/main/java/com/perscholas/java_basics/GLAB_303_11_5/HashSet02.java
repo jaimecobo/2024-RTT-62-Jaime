647db17dceb497c59e51d4f8fbf1454fa2a2a056
Jaime <jaimecobo@outlook.com>
2024-05-12 15:55:14 -0500

    HashSet02, union between two HashSets

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/HashSet02.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/HashSet02.java
@@ -1,2 +1,25 @@
-package com.perscholas.java_basics.GLAB_303_11_5;public class HashSet02 {
+package com.perscholas.java_basics.GLAB_303_11_5;
+import java.util.HashSet;
+
+public class HashSet02 {
+    public static void main(String[] args) {
+/*
+Union of Sets.
+To perform the union between two sets, we can use the addAll() method.
+*/
+        HashSet<Integer> evenNumbers = new HashSet<>();
+        evenNumbers.add(2);
+        evenNumbers.add(4);
+        System.out.println("HashSet1: " + evenNumbers);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(3);
+        System.out.println("HashSet2: " + numbers);
+
+        // Union of two set
+        numbers.addAll(evenNumbers);
+        System.out.println("Union is: " + numbers);
+    }
+
 }
