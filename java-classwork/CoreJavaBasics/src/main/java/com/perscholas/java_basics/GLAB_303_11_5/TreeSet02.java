8c535360bdc1ca182929ce76639776e219a414cf
Jaime <jaimecobo@outlook.com>
2024-05-14 20:41:59 -0500

    TreeSet02

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/TreeSet02.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_5/TreeSet02.java
@@ -1,2 +1,27 @@
-package com.perscholas.java_basics.GLAB_303_11_5;public class TreeSet02 {
+package com.perscholas.java_basics.GLAB_303_11_5;
+
+import java.util.TreeSet;
+
+public class TreeSet02 {
+/*
+Remove Elements
+remove() - removes the specified element from the set.
+removeAll() - removes all of the elements from the set.
+ */
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using the remove() method
+        boolean value1 = numbers.remove(5);
+        System.out.println("Is 5 removed? " + value1);
+
+        // Using the removeAll() method
+        boolean value2 = numbers.removeAll(numbers);
+        System.out.println("Are all elements removed? " + value2);
+    }
+
 }
