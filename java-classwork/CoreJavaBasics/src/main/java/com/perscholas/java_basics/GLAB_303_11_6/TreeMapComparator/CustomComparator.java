2767287c211b2d92c482f26d0b27e6e3f2783a27
Jaime <jaimecobo@outlook.com>
2024-05-15 19:30:05 -0500

    CustomComparator

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_6/TreeMapComparator/CustomComparator.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/GLAB_303_11_6/TreeMapComparator/CustomComparator.java
@@ -1,2 +1,20 @@
-package com.perscholas.java_basics.GLAB_303_11_6.TreeMapComparator;public class CustomComparator {
+package com.perscholas.java_basics.GLAB_303_11_6.TreeMapComparator;
+import java.util.Comparator;
+public  class CustomComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String number1, String number2) {
+        int value =  number1.compareTo(number2);
+//      Elements are sorted in reverse order
+        if (value > 0) {
+            return -1;
+        }
+        else if (value < 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
+
