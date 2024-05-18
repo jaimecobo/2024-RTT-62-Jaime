f24b1cf4da9759dc114596daaf6f55732f265a18
Jaime <jaimecobo@outlook.com>
2024-05-17 21:53:26 -0500

    Human.java

---
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/Human.java
@@ -0,0 +1,8 @@
+package com.perscholas.java_basics.slides_303_14;
+
+@FunctionalInterface
+public interface Human {
+    void say(); /* A Single-Abstract-Method */
+
+}
+
---
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/MyFunctionalInterfaceKBA.java
@@ -0,0 +1,2 @@
+package com.perscholas.java_basics.slides_303_14;public class MyFunctionalInterfaceKBA {
+}
