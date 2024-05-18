ea06867c0dde4946b07e5f19c67f240170efe070
Jaime <jaimecobo@outlook.com>
2024-05-17 21:53:59 -0500

    NormalClass.java

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/NormalClass.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/NormalClass.java
@@ -1,2 +1,20 @@
-package com.perscholas.java_basics.slides_303_14;public class NormalClass {
+package com.perscholas.java_basics.slides_303_14;
+
+public class NormalClass {
+    /* Define an anonymous inner class implementing the interface.*/
+    /*Construct an instance and invoke the method. */
+    /* anonymous constructor one */
+    Human hObjone = new Human() {
+        @Override
+        public void say() {
+            System.out.println("I am James");
+        }
+    };
+    /* anonymous constructor two */
+    Human hObjTwo = new Human() {
+        @Override
+        public void say() {
+            System.out.println("i am Tony");
+        }
+    };
 }
