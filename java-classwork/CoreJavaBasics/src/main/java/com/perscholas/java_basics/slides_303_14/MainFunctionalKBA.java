3ac3f105551af03477a13986a2c0bbc32aaec80b
Jaime <jaimecobo@outlook.com>
2024-05-17 21:56:04 -0500

    MainFunctionalKBA.java

--- java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/MainFunctionalKBA.java
+++ java-classwork/CoreJavaBasics/src/main/java/com/perscholas/java_basics/slides_303_14/MainFunctionalKBA.java
@@ -1,2 +1,19 @@
-package com.perscholas.java_basics.slides_303_14;public class MainFunctionalKBA {
+package com.perscholas.java_basics.slides_303_14;
+
+public class MainFunctionalKBA {
+    public static void main(String[] args) {
+        // this and the implementation below are the same
+        MyFunctionalInterfaceKBA fi = (n) -> {
+            return n * n;
+        };
+
+//        MyFunctionalInterfaceKBA fi1 = new MyFunctionalInterfaceKBA() {
+//            @Override
+//            public Integer sqr(int a) {
+//                return a * a;
+//            }
+//        };
+
+        System.out.println(fi.sqr(5));
+    }
 }
