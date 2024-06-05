2bcb4f9854f5db101df92f1a0154f3944a74c5f5
Jaime <jaimecobo@outlook.com>
2024-06-04 22:17:47 -0500

    findByFirstName

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/FirstHibernateDemo.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/FirstHibernateDemo.java
@@ -1,2 +1,20 @@
-package com.perscholas.module305.JDBCDemo;public class FirstHibernateDemo {
+package com.perscholas.module305.JDBCDemo;
+
+import com.perscholas.module305.JDBCDemo.database.dao.EmployeeDAO;
+import com.perscholas.module305.JDBCDemo.database.entity.Employee;
+
+import java.util.List;
+
+public class FirstHibernateDemo {
+    public static void main(String[] args) {
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+
+        List<Employee> employees = employeeDAO.findByFirstName("Leslie");
+        for(Employee employee : employees){
+            System.out.println(employee.getId() + " | " + employee.getFirstname() + " | " + employee.getLastname() + " | " +
+                    employee.getEmail() + " | " + employee.getJobTitle());
+        }
+
+    }
+
 }
