c7f169ec23962774e21e01daa31e49c9248c0743
Jaime <jaimecobo@outlook.com>
2024-06-05 14:33:26 -0500

    Added createCustomer method

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
@@ -9,4 +9,30 @@ public class CustomerHibernateDemo {
 
     }
 
+
+    public static void createCustomer(CustomerDAO customerDAO){
+        // Let's create a new employee in the database
+        Customer newCustomer = new Customer();
+        newCustomer.setCustomerName("Per Scholas");
+        newCustomer.setContactFirstname("Eric");
+        newCustomer.setContactLastname("Heilig");
+        newCustomer.setPhone("7189918400");
+        newCustomer.setAddressLine1("804 E 138th St");
+        newCustomer.setAddressLine2(null);
+        newCustomer.setCity("Bronx");
+        newCustomer.setState("NY");
+        newCustomer.setPostalCode("10454");
+        newCustomer.setCountry("USA");
+        newCustomer.setSalesRepEmployeeId(1188);
+        newCustomer.setCreditLimit(null);
+
+        customerDAO.insert(newCustomer);
+
+        List<Customer> customerList = customerDAO.findByContactFirstName("Eric");
+        for(Customer customer : customerList){
+            System.out.println(customer);
+        }
+
+    }
+
 }
