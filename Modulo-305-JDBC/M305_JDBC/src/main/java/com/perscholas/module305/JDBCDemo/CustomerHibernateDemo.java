64773a95ce25589138af370149fa9f887be57696
Jaime <jaimecobo@outlook.com>
2024-06-05 14:33:45 -0500

    Tested createCustomer method

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
@@ -6,6 +6,7 @@ public class CustomerHibernateDemo {
 
     public static void main(String[] args) {
         CustomerDAO customerDAO = new CustomerDAO();
+        createCustomer(customerDAO);
 
     }
 
