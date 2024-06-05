ec2afa784427d296f630387ca96b0b1165656a1e
Jaime <jaimecobo@outlook.com>
2024-06-05 14:32:25 -0500

    imported CustomerDAO

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/CustomerHibernateDemo.java
@@ -1,5 +1,7 @@
 package com.perscholas.module305.JDBCDemo;
 
+import com.perscholas.module305.JDBCDemo.database.dao.CustomerDAO;
+
 public class CustomerHibernateDemo {
 
     public static void main(String[] args) {
