e1cd4945ade2f58b06d2df8f07b4aa335b9044de
Jaime <jaimecobo@outlook.com>
2024-06-06 18:26:44 -0500

    Created a new OrderHibernateMain class

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
@@ -1,2 +1,12 @@
-package com.perscholas.module305.JDBCDemo;public class OrderHibernateMain {
+package com.perscholas.module305.JDBCDemo;
+
+import com.perscholas.module305.JDBCDemo.database.dao.OrderDAO;
+
+import java.util.Scanner;
+
+public class OrderHibernateMain {
+    private OrderDAO orderDAO = new OrderDAO();
+    Scanner scanner = new Scanner(System.in);
+
+
 }
