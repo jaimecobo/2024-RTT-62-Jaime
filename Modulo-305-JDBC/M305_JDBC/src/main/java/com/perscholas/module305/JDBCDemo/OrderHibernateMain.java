7fd378bc07a90ff0c1334881cc9370e4dd98b366
Jaime <jaimecobo@outlook.com>
2024-06-06 18:27:39 -0500

    Added required methods to  OrderHibernateMain class

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
@@ -1,6 +1,7 @@
 package com.perscholas.module305.JDBCDemo;
 
 import com.perscholas.module305.JDBCDemo.database.dao.OrderDAO;
+import com.perscholas.module305.JDBCDemo.database.entity.Order;
 
 import java.util.Scanner;
 
@@ -8,5 +9,59 @@ public class OrderHibernateMain {
     private OrderDAO orderDAO = new OrderDAO();
     Scanner scanner = new Scanner(System.in);
 
+
+    public static void main(String[] args) {
+        OrderHibernateMain orderHibernateMain = new OrderHibernateMain();
+        orderHibernateMain.run();
+
+    }
+
+
+    public void run(){
+        CustomerHibernateMain customerHibernateMain = new CustomerHibernateMain();
+        customerHibernateMain.printOrdersByCustomerId();
+
+        int orderId = 0;
+        Order selectedOrderNullHandling = null;
+        while(selectedOrderNullHandling == null){
+            orderId = promptForOrderId();
+            selectedOrderNullHandling = orderDAO.findById(orderId);
+            if(selectedOrderNullHandling == null) {
+                System.out.println("You have entered a nonexistent id number, please try again!");
+            }
+        }
+
+        System.out.println("Please type in the comment you want to add to order " + orderId + ": ");
+        String commentToAdd = scanner.nextLine();
+
+        updateCommentsInOrder(selectedOrderNullHandling, commentToAdd);
+
+        System.out.println(" Order id \t|\t Order date \t|\t Required date \t|\t Shipped date \t|\t Status \t|\t Comments" );
+        System.out.println("=========================================================================================================");
+        System.out.println("\t" + selectedOrderNullHandling.getId() + " \t|\t " + selectedOrderNullHandling.getOrderDate() + " \t|\t " +
+                selectedOrderNullHandling.getRequiredDate()  + " \t|\t " + selectedOrderNullHandling.getShippedDate()  + " \t|\t " +
+                selectedOrderNullHandling.getStatus() + " \t|\t " + selectedOrderNullHandling.getComments());
+
+    }
+
+
+    public void updateCommentsInOrder(Order order, String comment){
+        order.setComments(comment);
+        orderDAO.update(order);
+    }
+
+
+    public int promptForOrderId() {
+        while(true) {
+            try {
+                System.out.print("Enter the order id to modify: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                return id;
+            } catch (Exception e) {
+                System.out.println("Please enter a valid id number: ");
+                scanner.nextLine();
+            }
+        }
+    }
 
 }
