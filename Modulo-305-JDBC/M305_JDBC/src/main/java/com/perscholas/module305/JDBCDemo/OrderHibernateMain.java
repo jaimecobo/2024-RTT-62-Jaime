f8edc09007a6728030ca5ec4896289af3742e905
Jaime <jaimecobo@outlook.com>
2024-06-06 23:40:53 -0500

    Added the print out section removed from CustomerHibernateMain_printOrderByCustomerId and also improved/limited the search of the orderID to the List of orders of the queried customer

--- Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
+++ Modulo-305-JDBC/M305_JDBC/src/main/java/com/perscholas/module305/JDBCDemo/OrderHibernateMain.java
@@ -3,6 +3,7 @@ package com.perscholas.module305.JDBCDemo;
 import com.perscholas.module305.JDBCDemo.database.dao.OrderDAO;
 import com.perscholas.module305.JDBCDemo.database.entity.Order;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class OrderHibernateMain {
@@ -18,24 +19,51 @@ public class OrderHibernateMain {
 
 
     public void run(){
+        // Calls printOrdersByCustomerId from CustomerHibernateMain to get the List of orders of a customer
         CustomerHibernateMain customerHibernateMain = new CustomerHibernateMain();
-        customerHibernateMain.printOrdersByCustomerId();
+        List<Order> orders = customerHibernateMain.printOrdersByCustomerId();
+        System.out.println(" Order id \t|\t Order date \t|\t Required date \t|\t Shipped date \t|\t Status \t|\t Comments" );
+        System.out.println("=========================================================================================================");
+        for(Order order : orders){
+            System.out.println("\t" + order.getId() + " \t|\t " + order.getOrderDate() + " \t|\t " + order.getRequiredDate()  + " \t|\t " +
+                    order.getShippedDate()  + " \t|\t " + order.getStatus() + " \t|\t " + order.getComments());
+        }
 
+
+        // This piece of code allows you to enter any valid order id and looks for it in the entire orders table
+//        int orderId = 0;
+//        Order selectedOrderNullHandling = null;
+//        while(selectedOrderNullHandling == null){
+//            orderId = promptForOrderId();
+//            selectedOrderNullHandling = orderDAO.findById(orderId);
+//            if(selectedOrderNullHandling == null) {
+//                System.out.println("You have entered a nonexistent id number, please try again!");
+//            }
+//        }
+
+        // This piece of code limits the order id input to the orders in to List of orders of the specific customer queried before
         int orderId = 0;
         Order selectedOrderNullHandling = null;
         while(selectedOrderNullHandling == null){
             orderId = promptForOrderId();
-            selectedOrderNullHandling = orderDAO.findById(orderId);
+            for(Order order : orders) {
+                if (order.getId() == orderId) {
+                    selectedOrderNullHandling = orderDAO.findById(orderId);
+                }
+            }
             if(selectedOrderNullHandling == null) {
                 System.out.println("You have entered a nonexistent id number, please try again!");
             }
         }
 
+        // After having a valid order id input, the user is asked to type in the comment for the order
         System.out.println("Please type in the comment you want to add to order " + orderId + ": ");
         String commentToAdd = scanner.nextLine();
 
+        // Having the order and the comment this line of code updates the database table
         updateCommentsInOrder(selectedOrderNullHandling, commentToAdd);
 
+        // Then we print out the order that we just merged into the table, to allow the user to visualize the changes
         System.out.println(" Order id \t|\t Order date \t|\t Required date \t|\t Shipped date \t|\t Status \t|\t Comments" );
         System.out.println("=========================================================================================================");
         System.out.println("\t" + selectedOrderNullHandling.getId() + " \t|\t " + selectedOrderNullHandling.getOrderDate() + " \t|\t " +
@@ -58,7 +86,7 @@ public class OrderHibernateMain {
                 int id = Integer.parseInt(scanner.nextLine());
                 return id;
             } catch (Exception e) {
-                System.out.println("Please enter a valid id number: ");
+                System.out.print("Please enter a valid id number: ");
                 scanner.nextLine();
             }
         }
