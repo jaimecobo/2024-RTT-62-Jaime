5e1a24b68f7f58c848e5cfde1264e08f897d1782
Jaime <jaimecobo@outlook.com>
2024-06-10 20:39:42 -0500

    CustomerDAOTest.java

--- Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/CustomerDAOTest.java
+++ Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/CustomerDAOTest.java
@@ -1,2 +1,11 @@
-package com.perscholas.module305.JDBCDemo;public class CustomerDAOTest {
+package com.perscholas.module305.JDBCDemo;
+
+import com.perscholas.module305.JDBCDemo.database.dao.CustomerDAO;
+import com.perscholas.module305.JDBCDemo.database.entity.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CustomerDAOTest {
+
 }
