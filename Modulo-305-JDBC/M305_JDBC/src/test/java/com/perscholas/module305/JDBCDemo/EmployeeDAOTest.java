7a42169b2ea03a48a8dbf31371356dbafafb9e3b
Jaime <jaimecobo@outlook.com>
2024-06-10 20:46:10 -0500

    EmployeeDAOTest.java + findByIdTest

--- Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/EmployeeDAOTest.java
+++ Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/EmployeeDAOTest.java
@@ -1,10 +1,28 @@
 package com.perscholas.module305.JDBCDemo;
 
 import com.perscholas.module305.JDBCDemo.database.dao.EmployeeDAO;
+import com.perscholas.module305.JDBCDemo.database.entity.Employee;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class EmployeeDAOTest {
 
     private static EmployeeDAO employeeDAO = new EmployeeDAO();
 
+        @Test
+    public void findByIdTest(){
+        // Given
+        Integer employeeId = 1002;
+        // When
+        Employee employee = employeeDAO.findById(employeeId);
+        // Then
+        Assertions.assertNotNull(employee);
+        Assertions.assertEquals(employeeId, employee.getId());
+        Assertions.assertEquals("Murphy", employee.getLastname());
+        Assertions.assertEquals("dmurphy@classicmodelcars.com", employee.getEmail());
+
+    }
 
 }
