a1a4612d7554325059872717db324b0565bbb6ff
Jaime <jaimecobo@outlook.com>
2024-06-10 20:45:39 -0500

    EmployeeDAOTest.java

--- Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/EmployeeDAOTest.java
+++ Modulo-305-JDBC/M305_JDBC/src/test/java/com/perscholas/module305/JDBCDemo/EmployeeDAOTest.java
@@ -1,2 +1,10 @@
-package com.perscholas.module305.JDBCDemo;public class EmployeeDAOTest {
+package com.perscholas.module305.JDBCDemo;
+
+import com.perscholas.module305.JDBCDemo.database.dao.EmployeeDAO;
+
+public class EmployeeDAOTest {
+
+    private static EmployeeDAO employeeDAO = new EmployeeDAO();
+
+
 }
